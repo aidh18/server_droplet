74f206c3c86f7b7a3374e23b3cc041aa3fef1b5d
Aidan Stacey <98003687+aidh18@users.noreply.github.com>
2024-03-27 10:04:46 -0600

    Matched db_access

--- src/server_droplet_sup.erl
+++ src/server_droplet_sup.erl
@@ -16,20 +16,39 @@
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
-%% sup_flags() = #{strategy => strategy(),         % optional
-%%                 intensity => non_neg_integer(), % optional
-%%                 period => pos_integer()}        % optional
+%% Configuration options for all the supervised children.
+    %% If a child process crashes, restart only that one (one_for_one).
+    %% If there is more than 2 crashes ('intensity') in
+    %% 3600 seconds ('period'), crash the supervisor and all
+    %% its children.
+    %%
+    %% sup_flags() = #{strategy => strategy(),         % optional
+    %%                 intensity => non_neg_integer(), % optional
+    %%                 period => pos_integer()}        % optional
+    SupFlags = #{strategy => one_for_all,
+    intensity => 2,
+    period => 3600},
+%% Specify a list of child processes. Each must including
+%% a unique id and the start function for the module.
+%% These children may be more supervisors, gen_servers,
+%% finite state machines (FSM), or other standard or custom
+%% OTP elements.
+ChildSpecList = [child(request_sup,supervisor)],
+{ok, {SupFlags, ChildSpecList}}.
+%% internal functions
+%%
+%%
+%%
+child(Module,Type)->
 %% child_spec() = #{id => child_id(),       % mandatory
 %%                  start => mfargs(),      % mandatory
 %%                  restart => restart(),   % optional
 %%                  shutdown => shutdown(), % optional
 %%                  type => worker(),       % optional
 %%                  modules => modules()}   % optional
-init([]) ->
-    SupFlags = #{strategy => one_for_all,
-                 intensity => 0,
-                 period => 1},
-    ChildSpecs = [],
-    {ok, {SupFlags, ChildSpecs}}.
-
-%% internal functions
+#{id => Module,
+start => {Module,start_link,[]},
+restart => permanent,
+shutdown => 2000,
+type => Type,
+modules => [Module]}.
