acc98bbfd3c408b6a3e9ece9cd09e85957aad339
Aidan Stacey <98003687+aidh18@users.noreply.github.com>
2024-03-27 10:41:29 -0600

    new

--- src/server_droplet_app.erl
+++ src/server_droplet_app.erl
@@ -1,5 +1,5 @@
 %%%-------------------------------------------------------------------
-%% @doc server_droplet public API
+%% @doc db_access public API
 %% @end
 %%%-------------------------------------------------------------------
 
@@ -13,12 +13,11 @@ start(_Type, _Args) ->
 	Dispatch = cowboy_router:compile([
 	    {'_', [
 		%{"/", cowboy_static, {priv_file, db_access, "static/index.html"}},
-	        {"/", toppage_h, []},
-            {"/package_transferred",package_transferred_h,[]},
-            {"/delivered",delivered_h,[]},
-            {"/location_request",location_request_h,[]},
-            {"/location_update",location_update_h,[]}
-
+			{"/", toppage_h, []},
+			{"/package_transferred",package_transferred_h,[]},
+			{"/delivered",delivered_h,[]},
+			{"/location_request",location_request_h,[]},
+			{"/location_update",location_update_h,[]}
 	    ]}
 	]),
 
@@ -26,10 +25,11 @@ start(_Type, _Args) ->
 	%tls stands for transport layer security
         {ok,_} = cowboy:start_tls(https_listener, [
                   		{port, 443},
-				{certfile, PrivDir ++ "/ssl/fullchain.pem"},
-				{keyfile, PrivDir ++ "/ssl/privkey.pem"}
-              		], #{env => #{dispatch => Dispatch}}),
+						{certfile, PrivDir ++ "/ssl/fullchain.pem"},
+						{keyfile, PrivDir ++ "/ssl/privkey.pem"}
+              			], #{env => #{dispatch => Dispatch}}),
 	db_access_sup:start_link().
+
 stop(_State) ->
     ok.
 
