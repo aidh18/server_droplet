b3e9d39cdd120e1da95bcad440621f70e1f57fe2
Aidan <jaidanstacey@gmail.com>
2024-04-04 14:19:19 -0600

    Fixed 500 error

--- src/location_request_h.erl
+++ src/location_request_h.erl
@@ -20,6 +20,11 @@ init(Req0, Opts) ->
 				<<"content-type">> => <<"text/json">>
 			}, Encoded_message, Req0),
 			{ok, Response, Opts};
+		Result =:= 500->
+			Response = cowboy_req:reply(200, #{
+				<<"content-type">> => <<"text/json">>
+			}, list_to_binary("500"), Req0),
+			{ok, Response, Opts};
 		true->
 			Response = cowboy_req:reply(200, #{
 				<<"content-type">> => <<"text/json">>
