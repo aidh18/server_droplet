7f3b8250b040ddd5b67350f39dee136e1fbe4ba6
Aidan <jaidanstacey@gmail.com>
2024-07-16 19:14:27 -0700

    testing

--- src/login_request_h.erl
+++ src/login_request_h.erl
@@ -9,24 +9,24 @@
 init(Req0,Opts) ->
 	{ok,Data,_} = cowboy_req:read_body(Req0),
 	#{<<"username">> := Username,<<"password">> := Password} = jsx:decode(Data),
-	Result = erpc:call(?SERVER,?LOGIC,request_login_api,[binary_to_list(Username),binary_to_list(Password)]),
+	Result = erpc:call(?SERVER,?LOGIC,request_login_api,[binary_to_term(Username),binary_to_term(Password)]),
 	io:format(Result),
 
 	if
 		is_list(Result)->
 			Response = cowboy_req:reply(200,#{
 				<<"content-type">> => <<"text/json">>
-			},list_to_binary(Result),Req0),
+			},term_to_binary(Result),Req0),
 			{ok,Response,Opts};
 		Result =:= 500->
 			Response = cowboy_req:reply(200,#{
 				<<"content-type">> => <<"text/json">>
-			},list_to_binary("500"),Req0),
+			},term_to_binary("500"),Req0),
 			{ok,Response,Opts};
 		true->
 			Response = cowboy_req:reply(200,#{
 				<<"content-type">> => <<"text/json">>
-			},list_to_binary("Invalid"),Req0),
+			},term_to_binary("Invalid"),Req0),
 			{ok,Response,Opts}
 	end.
 
--- src/settings_request_h.erl
+++ src/settings_request_h.erl
@@ -9,7 +9,7 @@
 init(Req0,Opts) ->
 	{ok,Data,_} = cowboy_req:read_body(Req0),
 	#{<<"user_id">> := User_id} = jsx:decode(Data),
-	Result = erpc:call(?SERVER,?LOGIC,request_settings_api,[binary_to_list(User_id)]),
+	Result = erpc:call(?SERVER,?LOGIC,request_settings_api,[binary_to_term(User_id)]),
 	io:format(Result),
 
 	if
@@ -22,11 +22,11 @@ init(Req0,Opts) ->
 		Result =:= 500->
 			Response = cowboy_req:reply(200,#{
 				<<"content-type">> => <<"text/json">>
-			},list_to_binary("500"),Req0),
+			},term_to_binary("500"),Req0),
 			{ok,Response,Opts};
 		true->
 			Response = cowboy_req:reply(200,#{
 				<<"content-type">> => <<"text/json">>
-			},list_to_binary(Result),Req0),
+			},term_to_binary(Result),Req0),
 			{ok,Response,Opts}
 	end.
